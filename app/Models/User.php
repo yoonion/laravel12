40e010d1b6e0c794f4c0f1429fc7969409ba2692
yoonion <yoonion97@gmail.com>
2025-12-13 19:04:53 +0900

    feat: user - post model relation define

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,11 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
